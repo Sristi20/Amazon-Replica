c33323da2230e5b81185b4cf221db3d4c45f1f6f
Sristi <sristi.shukla@krantitechservice.in>
2022-02-04 17:06:14 +0530

    product page and home page

---
+++ css/index.js
@@ -0,0 +1,31 @@
+var flag = 0;
+slideshow(flag);
+
+function controler(x){
+  flag = flag + x;
+  slideshow(flag);
+}
+
+function slideshow(n){
+ var slides = document.getElementsByClassName('slide');
+ if (n == slides.length){
+   flag = 0;
+   n = 0;
+ }
+if (n < 0){
+  flag = slides.length - 1;
+  n = slides.length -1;
+}
+ for (let y of slides){
+   y.style.display = "none"
+ }
+ slides[n].style.display = "block"
+
+}
+function openNav() {
+  document.getElementById("mySidebar").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+}
